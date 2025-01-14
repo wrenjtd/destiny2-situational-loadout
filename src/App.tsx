68d58d157fb9fae53e321df66fcebf91fa761fd7
Kevin Newton <wrenjtd@gmail.com>
2025-01-13 23:16:10 -0500

    Adding destiny server info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ClientOAuth2 from 'client-oauth2'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const clientSecret = import.meta.env.VITE_API_SECRET;
+  const clientID = import.meta.env.VITE_API_CLIENT_ID;
+  const base_auth_url = "https://www.bungie.net/en/OAuth/Authorize";
+  const redirect_uri = "https://github.com/wrenjtd/destiny2-siuational-loadout";
+  const token_url = "https://www.bungie.net/Platform/App/OAuth/token/";
+  const get_user_details_endpoint = "https://www.bungie.net/Platform/User/GetCurrentBungieNetUser/";
+
+ 
+  const session = new ClientOAuth2({
+    clientId: clientID,
+    clientSecret: clientSecret,
+    redirectUri: redirect_uri, 
+    authorizationUri: base_auth_url,
+    accessTokenUri: token_url,
+    scopes: ['ReadUserData']
+  }); 
+
+  //console.log("Authorization Link" + {session});
+
+  const token = session.createToken({
+    clientID : clientID,
+    clientSecret : clientSecret,
+    token_url : token_url
+  });
+
+  const additionalHeaders = {'X-API-Key': apiKey};
+  
+  token.sign({
+    method: 'GET',
+    url: get_user_details_endpoint,
+    headers: additionalHeaders
+  });
+
 
   return (
     <>
@@ -18,6 +52,7 @@ function App() {
       
     </>
   )
+  
 }
 
 export default App
