9f6dba300f54883ddfcd5b3103fd314cd6cde95a
Kevin Newton <wrenjtd@gmail.com>
2025-01-15 14:46:45 -0500

    Adding destiny server info

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import ClientOAuth2 from 'client-oauth2'
 
@@ -9,50 +9,65 @@ function App() {
   const clientSecret = import.meta.env.VITE_API_SECRET;
   const clientID = import.meta.env.VITE_API_CLIENT_ID;
   const base_auth_url = "https://www.bungie.net/en/OAuth/Authorize";
-  const redirect_uri = "https://github.com/wrenjtd/destiny2-siuational-loadout";
+  const redirect_uri = "https://github.com/wrenjtd/destiny2-situational-loadout";
   const token_url = "https://www.bungie.net/Platform/App/OAuth/token/";
-  const get_user_details_endpoint = "https://www.bungie.net/Platform/User/GetCurrentBungieNetUser/";
 
- 
+
   const session = new ClientOAuth2({
     clientId: clientID,
-    clientSecret: clientSecret,
-    redirectUri: redirect_uri, 
+    //clientSecret: clientSecret,
+    redirectUri: redirect_uri,
     authorizationUri: base_auth_url,
-    accessTokenUri: token_url,
-    scopes: ['ReadUserData']
-  }); 
-
-  //console.log("Authorization Link" + {session});
-
-  const token = session.createToken({
-    clientID : clientID,
-    clientSecret : clientSecret,
-    token_url : token_url
+    //accessTokenUri: token_url,
+    scopes: ['ReadUserData', 'ReadBasicUserProfile']
   });
 
-  const additionalHeaders = {'X-API-Key': apiKey};
+
+  const get_user_details_endpoint = "https://www.bungie.net/Platform/User/GetCurrentBungieNetUser/";
+
+  const token = session.credentials.getToken();
+  console.log(token);
+
+  const additionalHeaders = { 'X-API-Key': apiKey, 'Host': 'localhost:5173' };
+
+
+  // token.expiresIn(3600);
   
-  token.sign({
-    method: 'GET',
-    url: get_user_details_endpoint,
-    headers: additionalHeaders
-  });
+  // token.sign({
+  //   method: 'GET',
+  //   url: get_user_details_endpoint,
+  //   headers: additionalHeaders
+  // });
 
+  const [eData, setEData] = useState("");
+
+  const getData = async () => {
+    let data = await fetch("https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", { headers: additionalHeaders });
+    console.log(data.json())
+
+  }
+
+
+  useEffect(() => {
+    let info = getData();
+    setEData(String(info));
+
+  }, [])
 
   return (
     <>
-      
+
       <div className="card">
-        <p>Click Me!</p>
+        <p>{eData}</p>
+        <p>Click Me</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      
+
     </>
   )
-  
+
 }
 
 export default App
